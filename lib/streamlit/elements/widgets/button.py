25e165390044acdd68b58841cdeda63d56c1e016
Bob Nisco <bob.nisco@snowflake.com>
2025-05-14 13:25:01 -0700

    A few quick fixes, still WIP

--- lib/streamlit/elements/widgets/button.py
+++ lib/streamlit/elements/widgets/button.py
@@ -1052,7 +1052,7 @@ class ButtonMixin:
 
         button_state = register_widget(
             button_proto.id,
-            on_change_handler=on_click,
+            callbacks={"click": on_click},
             args=args,
             kwargs=kwargs,
             deserializer=serde.deserialize,
